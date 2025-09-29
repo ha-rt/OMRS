89199b33be183a37dfdb9f0ea185b9b17b6d1012
Oliver <oliverm.development@proton.me>
2025-09-28 21:57:09 -0700

    feat: updated the news module to use news api

--- src/modules/news.py
+++ src/modules/news.py
@@ -1,20 +1,12 @@
-from openai import OpenAI
-from os import getenv
-from utils.yaml import get_yaml_safely
 from requests import get
 
-SYSTEM_PROMPT = get_yaml_safely("config/systemprompts.yaml")["news"]
-COUNTRY_CODE = get("https://ipapi.co/json/").json()["country"]
+COUNTRY_CODE = get("https://ipapi.co/json/").json().get("country")
 
-def get_news_as_json(client):
-    pass
-
-def get_news_as_summary(client: OpenAI):
-    ai_response = client.responses.create(
-        model="gpt-5",
-        reasoning={"effort": "low"},
-        instructions=SYSTEM_PROMPT,
-        input="to be inputted",
-    ).output_text
+def get_top_headlines(api_key: str):
+    top_articles = get(f"https://newsapi.org/v2/top-headlines?country=us&apiKey={api_key}").json()
     
-    return ai_response
+    if top_articles.get("status") != "ok":
+        return []
+    
+    headlines = [article['title'] for article in top_articles.get('articles', [])]
+    return headlines
